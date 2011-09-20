b01fe7fa8e2ad859696cf0525955ef4c280ac193
hradec <root@appletv2.(none)>
2011-09-20 20:35:10 -0300

    added patch for missing sound on dbt-t boards found at: http://www.lonelycoder.com/redmine/issues/673

--- src/parsers.c
+++ src/parsers.c
@@ -385,8 +385,16 @@ depacketize(service_t *t, elementary_stream_t *st, size_t len,
 
   plen = (buf[4] << 8) | buf[5];
 
-  if(plen + 6 > len || next_startcode != sc)
-    return 3;
+  if(plen + 6 > len || next_startcode != sc) {
+    if ( sc == 445 && next_startcode == 448 && plen + 5 < len ) {
+      tvhlog(LOG_DEBUG, "parser", "Depacketise: len %i, plen %i",
+          len, plen);
+      tvhlog(LOG_DEBUG, "parser", "Depacketise: sc %u, next_startcode %u",
+          sc, next_startcode);
+    } else {
+      return 3;
+    }
+  }
 
   if(plen + 6 < len)
     return 1;
